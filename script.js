b2118eb48a2b21dbeb72f442282de90e61824a29
changchangw <474038188@qq.com>
2025-07-22 22:04:35 +0100

    Implement the beginning and the room1 flow

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const scenes = {
   1: { image: 'assets/chapter1/start.png', action: () => goToScene(2) },
-  2: { image: 'assets/chapter1/prologue.png', action: () => goToScene(3) },
-  3: { image: 'assets/chapter1/cover.png', action: () => goToScene(4) },
+  2: { image: 'assets/chapter1/cover.png', action: () => goToScene(3) },
+  3: { image: 'assets/chapter1/prologue.png', action: () => goToScene(4) },
   4: {
     image: 'assets/chapter1/rooms.png',
     hotspots: [
@@ -29,6 +29,10 @@ const scenes = {
       {
         left: '1216px', top: '340px', width: '40px', height: '40px', // arrow →
         action: () => goToScene(6)
+      },
+      {
+        left: '476px', top: '108px', width: '165px', height: '130px', // Graduation Photo
+        action: () => goToScene(7)
       }
     ]
   },
@@ -38,9 +42,31 @@ const scenes = {
       {
         left: '24px', top: '340px', width: '40px', height: '40px', // arrow ←
         action: () => goToScene(5)
+      },
+      {
+        left: '117px', top: '102px', width: '486px', height: '278px', // Shakespeare's Sonnets
+        action: () => goToScene(11)
+      },
+      {
+        left: '756px', top: '353px', width: '350px', height: '126px', // Flawless
+        action: () => goToScene(15)
       }
     ]
-  }
+  },
+  7: { image: 'assets/chapter1/room1/1-1.png', action: () => goToSceneInstant(8) },
+  8: { image: 'assets/chapter1/room1/1-2.png', action: () => goToSceneInstant(9) },
+  9: { image: 'assets/chapter1/room1/1-3.png', action: () => goToSceneInstant(10) },
+  10: { image: 'assets/chapter1/room1/1-4.png', action: () => goToScene(5) },
+
+  11: { image: 'assets/chapter1/room1/2-1.png', action: () => goToSceneInstant(12) },
+  12: { image: 'assets/chapter1/room1/2-2.png', action: () => goToSceneInstant(13) },
+  13: { image: 'assets/chapter1/room1/2-3.png', action: () => goToSceneInstant(14) },
+  14: { image: 'assets/chapter1/room1/2-4.png', action: () => goToScene(6) },
+
+  15: { image: 'assets/chapter1/room1/3-1.png', action: () => goToSceneInstant(16) },
+  16: { image: 'assets/chapter1/room1/3-2.png', action: () => goToSceneInstant(17) },
+  17: { image: 'assets/chapter1/room1/3-3.png', action: () => goToSceneInstant(18) },
+  18: { image: 'assets/chapter1/room1/3-4.png', action: () => goToScene(6) },
 };
 
 let currentScene = 1;
@@ -61,6 +87,12 @@ function goToScene(index) {
       overlay.style.opacity = 0; // 变亮
     }, 50);
   }, 600); // 等待黑屏完成后切换
+}
+
+function goToSceneInstant(index) {
+  currentScene = index;
+  sceneImage.src = scenes[index].image;
+  updateHotspots();
 }
 
 function updateHotspots() {
